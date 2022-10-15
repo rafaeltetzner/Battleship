080ef493f39603cbb815f574e7a09b9368f55cb8
rafaeltetzner <rafaelaraujotetzner@gmail.com>
2022-10-15 13:27:47 -0300

    Major changes

--- battle_map.py
+++ battle_map.py
@@ -1,37 +1,64 @@
 import numpy as np
+from ship import Ship
+from tilemap import TileMap
 
 
 class BattleMap:
     def __init__(self):
-        self.tile_map_data = np.chararray([100], unicode=True)
-        self.tile_map_data[:] = ' '
-        print(self.tile_map_data)
-        self.tile_map = """
-           1   2   3   4   5   6   7   8   9   10
-         +---+---+---+---+---+---+---+---+---+---+
-        A| {0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8} | {9} |
-         +---+---+---+---+---+---+---+---+---+---+
-        B| {10} | {11} | {12} | {13} | {14} | {15} | {16} | {17} | {18} | {19} |
-         +---+---+---+---+---+---+---+---+---+---+
-        C| {20} | {21} | {22} | {23} | {24} | {25} | {26} | {27} | {28} | {29} |
-         +---+---+---+---+---+---+---+---+---+---+
-        D| {30} | {31} | {32} | {33} | {34} | {35} | {36} | {37} | {38} | {39} |
-         +---+---+---+---+---+---+---+---+---+---+
-        E| {40} | {41} | {42} | {43} | {44} | {45} | {46} | {47} | {48} | {49} |
-         +---+---+---+---+---+---+---+---+---+---+
-        F| {50} | {51} | {52} | {53} | {54} | {55} | {56} | {57} | {58} | {59} |
-         +---+---+---+---+---+---+---+---+---+---+
-        G| {60} | {61} | {62} | {63} | {64} | {65} | {66} | {67} | {68} | {69} |
-         +---+---+---+---+---+---+---+---+---+---+
-        H| {70} | {71} | {72} | {73} | {74} | {75} | {76} | {77} | {78} | {79} |
-         +---+---+---+---+---+---+---+---+---+---+
-        I| {80} | {81} | {82} | {83} | {84} | {85} | {86} | {87} | {88} | {89} |
-         +---+---+---+---+---+---+---+---+---+---+
-        J| {90} | {91} | {92} | {93} | {94} | {95} | {96} | {97} | {98} | {99} |
-         +---+---+---+---+---+---+---+---+---+---+
-        """
+        self.my_view = TileMap()
+        self.enemy_view = TileMap()
+        self.carrier = Ship(5, 2, "A0", 'C')
+        self.destroyer = Ship(6, 1, "A2", 'D')
+        self.battleship = Ship(5, 1, "A3", 'B')
+        self.cruiser = Ship(4, 1, "A4", 'c')
+        self.submarine = Ship(4, 1, "A5", 'S')
+        self.patrol = Ship(3, 1, "A6", 'P')
+        self.ships = [self.carrier, self.destroyer, self.battleship, self.cruiser, self.submarine, self.patrol]
 
-    def draw(self):
-        print(self.tile_map.format(*self.tile_map_data))
+    def place_carrier(self, coord):
+        return self.my_view.place_ship(coord, self.carrier)
 
-    
+    def place_destroyer(self, coord):
+        return self.my_view.place_ship(coord, self.destroyer)
+
+    def place_battleship(self, coord):
+        return self.my_view.place_ship(coord, self.battleship)
+
+    def place_cruiser(self, coord):
+        return self.my_view.place_ship(coord, self.carrier)
+
+    def place_submarine(self, coord):
+        return self.my_view.place_ship(coord, self.submarine)
+
+    def place_patrol(self, coord):
+        return self.my_view.place_ship(coord, self.patrol)
+
+    def draw4enemy(self):
+        self.enemy_view.draw()
+
+    def draw4me(self):
+        self.my_view.draw()
+
+    # Method returns true when a valid tile was hit
+    # False otherwise
+    def receive_bomb(self, coord) -> bool:
+        # Shoot at the same tile twice
+        if self.my_view.get(coord) == 'X':
+            return False
+
+        symbol4enemy = self.my_view.get(coord)
+        if symbol4enemy == ' ':
+            symbol4enemy = 'X'
+        self.enemy_view.set(coord, symbol4enemy)
+        self.my_view.set(coord, 'X')
+        for ship in self.ships:
+            if ship.contains(coord):
+                ship.hit()
+                break
+        return True
+
+    def check_if_lost(self):
+        for ship in self.ships:
+            if ship.is_alive():
+                return False
+        return True
---
+++ ship.py
@@ -0,0 +1,29 @@
+class Ship:
+    def __init__(self, length, width, coord, symbol):
+        self.length = length
+        self.width = width
+        self.coord = coord
+        self.symbol = symbol
+        self.num_hits = 0
+
+    def fits_at(self, coord):
+        if chr(self.length + ord(coord[0]) - 1) > 'J':
+            return False
+        if chr(self .width + ord(coord[1]) - 1) > '9':
+            return False
+        return True
+
+    def contains(self, coord) -> bool:
+        if coord[0] < self.coord[0] or coord[0] > chr(ord(self.coord[0]) + self.length):
+            return False
+        if coord[1] < self.coord[1] or coord[1] > chr(ord(self.coord[1]) + self.length):
+            return False
+        return True
+
+    def hit(self):
+        self.num_hits += 1
+
+    def is_alive(self):
+        return self.num_hits < self.length * self.width
+
+
