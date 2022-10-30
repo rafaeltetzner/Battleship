477bfb3c1f210f82ccad1b7ab8b9408270b874c2
Gabriel-de-AvelarR <gabrieldeavelar@usp.br>
2022-10-30 11:56:47 -0300

    Falta implementar a atualizacao do mapa dos dois jogadores

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import socket
-import select
 
 # Python versão 3.8.10 (Colocar assert com mensagem de warning)
 
@@ -10,6 +9,12 @@ class Cliente():
     def __init__(self):
 
         self.cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        #logica para passagem de mensagem
+        self.LOCALIZACAO_ERRADA = 'LE'
+        self.ESPERA_VEZ= 'EV'
+        self.SUA_VEZ= 'SV'
+        self.FIM_DE_JOGO= 'FJ'
 
         self.SERVER_IP = socket.gethostname()
         self.SERVER_PORT = 1234 
@@ -30,9 +35,10 @@ class Cliente():
         except:
             return False                                         
 
-    def client_send(self):
-        self.client.send(mensagem.encode('utf-8'))
-
+    def client_send(self, mensagem):
+        # Ver se tem a chance da socket estar fechada ? Se estiver, matar cliente
+        self.cliente.send(mensagem.encode('utf-8'))
+        
     def run(self):
         
         # No comeco mesmo tem que saber onde instanciar as naves
@@ -52,15 +58,42 @@ class Cliente():
 
                     coord = input("> Onde voce quer atacar? (ex A2): ").upper()
 
-                    if not self.validade_input_format_format(coord):
+                    if not self.validate_input_format(coord):
                         print(">> Input invalido")
                         continue
+
                     # Envia para o servidor
-                    self.client_send()
-                    # Se servidor devolver um LE --> Printa localizacao errada, continue
-                    # Se servidor devolver um EV --> Espere sua vez, continue
-                    # Se servidor devolver um SV --> Sua vez, inputValido = True
-                    # Se servidor devolver um FJ --> Fim do jogo, inputValido = True, fimDoJogo = True
+                    self.client_send(coord)
+                    feedback = self.receive_message()
+
+                    if not feedback:
+                        pass
+
+                    elif(feedback == self.LOCALIZACAO_ERRADA):
+                        print(">> Localizacao invalida")
+                        continue
+
+                    elif(feedback == self.ESPERA_VEZ):
+                        print(">> Nao eh sua vez de jogar ainda")
+                        continue
+
+                    elif(feedback == self.SUA_VEZ):
+                        inputValido = True
+
+                        # Como chegou aqui, quer dizer que acabou de acabar a jogada
+                        # do outro player. Isso implica que ALGO foi atingido, logo
+                        # atualizar battle_map_my_view para ter agora o ponto novo 
+                        # atingido, de forma que quando voltar no loop para cima
+                        # (draw4me) temos que ter algo consistente
+                        coordAtingida = self.receive_message() 
+                        #self.battle_map_my_view.atualiza(coordAtingida)
+
+                    elif(feedback == self.FIM_DE_JOGO):
+                        inputValido = True 
+                        fimDoJogo = True
+
+                    else: # Cabuloso, server enviou algo válido mas que não está previsto, na duvida continua
+                        continue
 
             # Quando chegar algo do tipo SIGKILL
             except:
@@ -70,7 +103,7 @@ class Cliente():
                 pass
         
 
-    def validade_input_format_format(coord):
+    def validate_input_format(coord):
         if len(coord) > 2:
             return False
         if coord[0] < 'A' or coord[0] > 'J':
@@ -82,55 +115,55 @@ class Cliente():
     def init_ship(self):
         # self.battle_map_view.draw4me()
         while True:
-            coord = input("> Where would you like to place the carrier? (ex A2): ").upper()
-            if not self.validade_input_format(coord):
-                print(">> Invalid input")
+            coord = input("> Onde voce gostaria de posicionar o porta-avioes? (ex A2): ").upper()
+            if not self.validate_input_format(coord):
+                print(">> Input invalido")
             elif not self.battle_map_view.place_carrier(coord):
-                print(">> The carrier doesn't fit at this coordinates")
+                print(">> O porta-avioes nao se encaixa nessa posicao")
             else:
                 break
         #self.battle_map_view.draw4me()
         while True:
-            coord = input("> Where would you like to place the destroyer? (ex A2): ").upper()
-            if not self.validade_input_format(coord):
-                print(">> Invalid input")
+            coord = input("> Onde voce gostaria de posicionar o destruidor? (ex A2): ").upper()
+            if not self.validate_input_format(coord):
+                print(">> Input invalido")
             elif not self.battle_map_view.place_destroyer(coord):
-                print(">> The destroyer doesn't fit at this coordinates")
+                print(">> O destruidor nao se encaixa nessa posicao")
             else:
                 break
         self.battle_map_view.draw4me()
         while True:
-            coord = input("> Where would you like to place the battleship? (ex A2): ").upper()
-            if not self.validade_input_format(coord):
-                print(">> Invalid input")
+            coord = input("> Onde voce gostaria de posicionar o navio de batalha? (ex A2): ").upper()
+            if not self.validate_input_format(coord):
+                print(">> Input invalido")
             elif not self.battle_map_view.place_battleship(coord):
-                print(">> The battleship doesn't fit at this coordinates")
+                print(">> O navio de batalha nao se encaixa nessa posicao")
             else:
                 break
         # self.battle_map_view.draw4me()
         while True:
-            coord = input("> Where would you like to place the cruiser? (ex A2): ").upper()
-            if not self.validade_input_format(coord):
-                print(">> Invalid input")
+            coord = input("> Onde voce gostaria de posicionar o cruzador? (ex A2): ").upper()
+            if not self.validate_input_format(coord):
+                print(">> Input invalido")
             elif not self.battle_map_view.place_cruiser(coord):
-                print(">> The cruiser doesn't fit at this coordinates")
+                print(">> O cruzador nao se encaixa nessa posicao")
             else:
                 break
         # self.battle_map_view.draw4me()
         while True:
-            coord = input("> Where would you like to place the submarine? (ex A2): ").upper()
-            if not self.validade_input_format(coord):
-                print(">> Invalid input")
+            coord = input("> Onde voce gostaria de posicionar o submarino? (ex A2): ").upper()
+            if not self.validate_input_format(coord):
+                print(">> Input invalido")
             elif not self.battle_map_view.place_submarine(coord):
-                print(">> The submarine doesn't fit at this coordinates")
+                print(">> O submarino nao se encaixa nessa posicao")
             else:
                 break
         # self.battle_map_view.draw4me()
         while True:
-            coord = input("> Where would you like to place the patrol? (ex A2): ").upper()
-            if not self.validade_input_format(coord):
-                print(">> Invalid input")
+            coord = input("> Onde voce gostaria de posicionar o patrulhador? (ex A2): ").upper()
+            if not self.validate_input_format(coord):
+                print(">> Input invalido")
             elif not self.battle_map_view.place_submarine(coord):
-                print(">> The patrol doesn't fit at this coordinates")
+                print(">> O patrulhador nao se encaixa nessa posicao")
             else:
                 break
--- server.py
+++ server.py
@@ -15,10 +15,16 @@ class Server:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+        self.LOCALIZACAO_ERRADA = 'LE'
+        self.ESPERA_VEZ= 'EV'
+        self.SUA_VEZ= 'SV'
+        self.FIM_DE_JOGO= 'FJ'
+
         self.CLIENT_IP = socket.gethostname()
-        self.QUEUE_SIZE = 1
+        self.QUEUE_SIZE = 2
         self.num_registered_players = 0
         self.clientSockets = {1: '', 2: ''}
+        self.clientSockets_lists = []
 
         self.MESSAGE_LENGHT =  2 #dois caracters definem a coordenada do espaço no mapa
 
@@ -30,7 +36,8 @@ class Server:
         client_socket, client_address = self.server_socket.accept()
         self.num_registered_players += 1
         self.clientSockets[self.num_registered_players] = client_socket
-    
+        self.clientSockets_lists.append(client_socket)
+
     def receive_message(self, turn):
 
         try:
@@ -47,15 +54,45 @@ class Server:
 
     def jogada(self, player1, player2, turn):
 
-        # Recebe as mensagens 
-        coord = self.receive_message(turn)
+        inputValido = False 
 
-        if(turn == 1):
-            player1.on_turn(player2, coord)
-        else:
-            player2.on_turn(player1, coord)
+        while not inputValido:
+
+            # Le das sockets disponíveis
+            read_sockets, _, exception_sockets = select.select(self.clientSockets_lists, [], self.clientSockets_lists)
+
+            # Itera pelos sockets notificados
+            for notified_socket in read_sockets:
+
+                # Verifica se a socket notificada vem do cliente correto
+                if(notified_socket == self.clientSockets[turn]):
+                    # Recebe as mensagens 
+                    coord = self.receive_message(turn)
+
+                    # Para chegar neste caso, tem-se que chegou uma mensagem vazia, termina programa
+                    if coord is False:
+                        # Avisa (?) os clientes que moio
+                        pass
+                        
+                    atual = player1 if turn == 1 else player2 
+                    inimigo = player1 if turn == 2 else player2 
+
+                    # Como é a vez deste cliente devemos checar no battlemap se a posicao de ataque ja foi marcada
+                    if not inimigo.battle_map.receive_bomb(coord):
+                        self.clientSockets[turn].send(self.LOCALIZACAO_ERRADA.encode('utf-8'))
+
+                    else: # Input realmente válido  
+                        atual.on_turn(inimigo, coord)
+                        inputValido = True 
+                        self.clientSockets[1 if turn == 2 else 2].send(self.SUA_VEZ.encode('utf-8'))
+                        self.clientSockets[turn].send(coord.encode('utf-8'))
+
+                # Player errado mandou solicitacao, avisar ele e voltar para o comeco
+                else:
+                    self.clientSockets[1 if turn == 2 else 2].send(self.ESPERA_VEZ.encode('utf-8'))
+
 
         # Manda para o outro player
-        self.server_socket.send(self.clientSockets[1 if turn == 2 else 2].encode('utf-8'))
+        # self.server_socket.send(self.clientSockets[1 if turn == 2 else 2].encode('utf-8'))
 
 
